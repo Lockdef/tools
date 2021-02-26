d19472ede72304bfa3059e427a70e5e309ae2651
lockdef <40243059+Lockdef@users.noreply.github.com>
2021-02-27 05:56:22 +0900

    ✨文字数カウンタ実装

--- pages/char_counter.vue
+++ pages/char_counter.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <textarea name="text" id="text" cols="30" rows="10"></textarea>
-    <p>単語数：{{count}}</p>
+    <textarea v-model="text" name="text" id="text" cols="30" rows="10"></textarea>
+    <p>文字数：{{text.length}}</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      text: ""
+    }
+  }
+})
+</script>
