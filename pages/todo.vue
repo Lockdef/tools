72f65bc2b76c9a17303f033662b0c7b1df79b27e
lockdef <locktki@icloud.com>
2021-03-02 19:18:04 +0900

    ✨Todo追加機能実装

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <p>新しくTODOを作成する：<input type="text" /><input type="submit" /></p>
-    <ul>
-      <li>todo <button>完了(削除)</button></li>
+    <p>
+      新しくTODOを作成する：
+      <input v-model="text" type="text" />
+      <input type="submit" @click="pushTodo" />
+    </p>
+    <ul v-for="todo in todos" :key="todo.id">
+      <li>{{ todo }} <button>完了</button></li>
     </ul>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      text: '',
+      todos: [],
+    }
+  },
+  methods: {
+    pushTodo() {
+      if (!this.text) return
+      this.todos.push(this.text)
+      this.text = ''
+    },
+  },
+})
+</script>
